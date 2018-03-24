eef62c6b2742caaa63a55e49f779987235fd67fa
Geetha <geetharam740@gmail.com>
2018-03-24 21:59:10 +0530

    JAva Projects

---
+++ Even.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Even {
+
+	public static void main(String[] args) {
+	 Scanner in= new Scanner(System.in);
+	 int a = in.nextInt();
+	 
+	 if(a%2==0)
+		 System.out.println("Even");
+	 else
+		 System.out.println("Odd");
+
+	}
+
+}
